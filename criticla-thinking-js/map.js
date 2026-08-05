1fbe92b83ed60b241a958138f76219cbb236420f
Khandaker-Mohyet <mohyet008@gmail.com>
2026-08-06 00:15:35 +0600

    map end

--- criticla-thinking-js/map.js
+++ criticla-thinking-js/map.js
@@ -8,3 +8,18 @@
 const course1 = {
     name: "Programing Hero"
 }
+const course2 = {
+    name: "Next level web development"
+}
+
+// const obj= {}
+
+// obj.nexLevel = {courseId: "level2"}
+// obj[course1] = {courseId: "level1"}
+
+const map = new Map()
+
+map.set(course1, {courseId: "level1"})
+map.set(course2, {courseId: "level2"})
+
+console.log(map)
