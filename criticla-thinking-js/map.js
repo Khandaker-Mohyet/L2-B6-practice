5d0fd4af8317dfc62f33f51f8754077cd58e419e
Khandaker-Mohyet <mohyet008@gmail.com>
2026-08-05 17:09:31 +0600

    module 2 start

---
+++ criticla-thinking-js/map.js
@@ -0,0 +1,10 @@
+// const obj = {
+//     nextLevel: {courseId : "Level1"},
+//     "Programing Hero": {courseId : "Level2"}
+// }
+
+// console.log(obj["Programing Hero"])
+
+const course1 = {
+    name: "Programing Hero"
+}
