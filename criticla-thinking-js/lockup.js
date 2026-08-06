6f53f77f8dface7219abfe4a33c651b68bec3124
Khandaker-Mohyet <mohyet008@gmail.com>
2026-08-06 17:22:26 +0600

    aggregating data

--- criticla-thinking-js/lockup.js
+++ criticla-thinking-js/lockup.js
@@ -55,4 +55,38 @@ const count = surveyResponses.reduce((table, response) => {
   return table;
 }, {})
 
-console.log(count)
+// console.log(count)
+
+//* Grouping and Aggregating Data
+
+// Scenario: You have a flat array of sales data, and you need to group the sales by category,
+// calculating the total revenue and the number of items sold for each.
+
+const sales = [
+  { category: "Electronics", item: "Laptop", price: 1200, quantity: 1 },
+  { category: "Books", item: "JS Basics", price: 30, quantity: 2 },
+  { category: "Electronics", item: "Mouse", price: 25, quantity: 2 },
+  { category: "Home", item: "Chair", price: 150, quantity: 1 },
+  { category: "Books", item: "React Deep Dive", price: 50, quantity: 1 },
+  { category: "Electronics", item: "Keyboard", price: 80, quantity: 1 },
+];
+
+
+const totalSalseByCategory = sales.reduce((table, sale) => {
+  const { category, price, quantity } = sale;
+
+  if (!table[category]) {
+    table[category] = {
+      totalRevenue: 0,
+      itemCount: 0
+    }
+  }
+
+  table[category].totalRevenue += price * quantity;
+  table[category].itemCount += quantity
+
+  return table;
+
+}, {})
+
+console.log(totalSalseByCategory)
