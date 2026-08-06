f04ac10ec33da0623017ce21428ce67436b95ada
Khandaker-Mohyet <mohyet008@gmail.com>
2026-08-06 12:12:10 +0600

    grooping data

--- criticla-thinking-js/lockup.js
+++ criticla-thinking-js/lockup.js
@@ -21,3 +21,38 @@ const post = lookupTable["p-103"]
 // console.log(post)
 
 // grooping data
+
+
+//* Grouping and Aggregating Data
+
+// Scenario: Count every survey and group by response
+
+//? input
+const surveyResponses = [
+  "A",
+  "C",
+  "B",
+  "A",
+  "B",
+  "B",
+  "C",
+  "A",
+  "B",
+  "D",
+  "A",
+  "C",
+  "B",
+  "A",
+];
+
+const count = surveyResponses.reduce((table, response) => {
+  if (table[response]) {
+    table[response] = table[response] + 1
+  } else {
+    table[response] = 1;
+  }
+
+  return table;
+}, {})
+
+console.log(count)
