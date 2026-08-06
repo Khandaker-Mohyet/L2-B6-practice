59d9cf0ccc75966e05daaee9965dd8aaff3e778e
Khandaker-Mohyet <mohyet008@gmail.com>
2026-08-06 11:47:52 +0600

    lockup data

---
+++ criticla-thinking-js/lockup.js
@@ -0,0 +1,23 @@
+// genarate a lookup table
+
+// Input
+const postsArray = [
+  { id: "p-101", title: "Intro to SQL", author: "Alex" },
+  { id: "p-102", title: "Data Structures in JS", author: "Beth" },
+  { id: "p-103", title: "Understanding Reduce", author: "Chris" },
+  { id: "p-104", title: "CSS Grid Tricks", author: "Alex" },
+];
+
+const lookupTable = postsArray.reduce((table, post) => {
+  table[post.id] = post;
+
+  return table;
+}, {})
+
+// console.log(lookupTable)
+
+const post = lookupTable["p-103"]
+
+// console.log(post)
+
+// grooping data
