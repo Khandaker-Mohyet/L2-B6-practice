b2b8408fda9daf05a01b7698925d3fa30580aab6
Khandaker-Mohyet <mohyet008@gmail.com>
2026-08-07 01:04:34 +0600

    normalization done

--- criticla-thinking-js/normalizing.js
+++ criticla-thinking-js/normalizing.js
@@ -18,6 +18,28 @@ const posts = [
   { id: 5, userId: 102, title: "Node.js streams" },
 ];
 
+
+const postByUserId = posts.reduce((table, post) => {
+  const { userId } = post
+
+  if (!table[userId]) {
+    table[userId] = [];
+  }
+
+  table[userId].push(post);
+  return table
+
+}, {})
+
+const userWithPosts = users.map((user)=>{
+  return{
+    ...user,
+    posts: postByUserId[user.id] || [],
+  }
+})
+
+console.log(userWithPosts)
+
 //? output
 // [
 //   { id: 101, name: 'Alice', posts: [ { id: 2, ... }, { id: 3, ... } ] },
