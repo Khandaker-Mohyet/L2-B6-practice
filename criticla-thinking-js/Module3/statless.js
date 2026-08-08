41bb60bcce51f2e7c5096dc78876706b7be939ac
Khandaker-Mohyet <mohyet008@gmail.com>
2026-08-08 17:50:13 +0600

    stateless vs stateful apply

--- criticla-thinking-js/Module3/statless.js
+++ criticla-thinking-js/Module3/statless.js
@@ -11,20 +11,63 @@
 // console.log(counter(3))
 // console.log(counter(2))
 
-const counter = {
-  count : 0,
+// const counter = {
+//   count : 0,
+
+//   add(amount){
+//     this.count = this.count + amount;
+//   },
+
+//   print(){
+//     console.log(this.count)
+//   }
+
+// }
+
+// counter.add(2)
+// counter.add(3)
+
+// counter.print()
+
+// const createCounter = () =>{
+//   let count = 0;
+
+//   return(amount)=>{
+//     count = count+amount;
+//     return count;
+//   }
+// }
+
+// const counter = createCounter();
+
+// console.log(counter(2))
+// console.log(counter(5))
+
+
+class Counter{
+  constructor (count){
+    this.count = count;
+  }
 
   add(amount){
     this.count = this.count + amount;
-  },
+  }
 
   print(){
     console.log(this.count)
   }
-
 }
 
-counter.add(2)
-counter.add(3)
+const counter1 = new Counter(10);
 
-counter.print()
+counter1.add(2)
+counter1.add(3)
+
+counter1.print()
+
+const counter2 = new Counter(10);
+
+counter2.add(20)
+counter2.add(30)
+
+counter2.print()
