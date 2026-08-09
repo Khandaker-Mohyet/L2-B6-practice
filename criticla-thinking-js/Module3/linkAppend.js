a5279b0da3f4977b6d00bba11275717bcea4f782
Khandaker-Mohyet <mohyet008@gmail.com>
2026-08-09 17:52:12 +0600

    module 3 doen

--- criticla-thinking-js/Module3/linkAppend.js
+++ criticla-thinking-js/Module3/linkAppend.js
@@ -39,11 +39,71 @@ class LinkedList {
     }
 
     this.length++;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index > this.length) {
+      console.error("toi says")
+      return undefined
+    }
+
+    if(index === 0){
+      return this.prepend(value);
+    }
+
+    if(index === this.length){
+      return this.append(value)
+    }
+
+    // fine the leading node
+
+    const leadingNode = this. _traversToIndex(index-1);
+    const holdingNode  = leadingNode.next;
+
+    const newNode = new Node(value)
+
+    leadingNode.next = newNode;
+    newNode.next = holdingNode;
+
+    this.length++;
+    
+
+
+  }
+
+  remove(index) {
+
+    if(index === 0){
+      this.head = this.head.next;
+
+      if(this.length === 1){
+        this.tail=null;
+      }
+    }
+
+    const leadingNode = this. _traversToIndex(index-1);
+    const nodeToRemove = leadingNode.next;
+    leadingNode.next = nodeToRemove.next;
+
+    if(leadingNode === null){
+      this.tail = leadingNode;
+    }
    }
 
-  insert() { }
+// helper function
+  _traversToIndex(index){
+    let count = 0;
+    let currentNode = this.head;
 
-  remove() { }
+    while (count !== index) {
+      currentNode = currentNode.next;
+      count++;
+    }
+
+    return currentNode;
+  }
+
+
 
   print() {
     const arr = []
@@ -61,11 +121,19 @@ class LinkedList {
 
 const linkedList = new LinkedList();
 
-linkedList.append(1)
-linkedList.append(2)
-linkedList.append(3)
-linkedList.prepend(10)
-linkedList.prepend(20)
-linkedList.prepend(30)
+linkedList.append("A")
+linkedList.append("B")
+linkedList.append("C")
+linkedList.print()
+linkedList.remove(2)
+
+
+// linkedList.insert(2, 200);
+
+
+// linkedList.prepend(10)
+// linkedList.prepend(20)
+// linkedList.prepend(30)
+// linkedList.insert(2, 100)
 
 linkedList.print()
