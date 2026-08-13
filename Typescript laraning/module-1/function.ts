d3f22e4c0b9c8bbcbc699682da1406d08be375a2
Khandaker-Mohyet <mohyet008@gmail.com>
2026-08-14 00:08:07 +0600

    method and arro

--- Typescript laraning/module-1/function.ts
+++ Typescript laraning/module-1/function.ts
@@ -1,11 +1,27 @@
 function addNormal (a: number, b: number) {
  return a + b;
 }
-console.log(addNormal(5, 10))
+// console.log(addNormal(5, 10))
 
 const addArrow = (a: number, b: number) => {
  return a + b;
 }
-console.log(addArrow(10, 10))
+// console.log(addArrow(10, 10))
 
 // object => function=> method
+
+const poorUser = {
+ name: "Khandaker",
+ balace: 500,   
+ addBalance(money: number) {
+  return this.balace + money;
+ }
+}
+
+// console.log(poorUser.addBalance(100))
+
+const arr: number[] = [1, 2, 3, 4, 5]; 
+
+const sqrArry = arr.map((ele:number):number=>{return ele * ele})
+
+console.log(sqrArry)
