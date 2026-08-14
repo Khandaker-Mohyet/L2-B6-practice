0f2a6e1f3dc99012b5897b5f9d0073ac029de258
Khandaker-Mohyet <mohyet008@gmail.com>
2026-08-15 02:08:05 +0600

    genaric function create

---
+++ Typescript laraning/module-2/genFunction.ts
@@ -0,0 +1,8 @@
+
+const arrayWighString = (value: string)=>[value];
+
+const arrayWithNumber = (value: number)=>[value];
+
+const arrayWithUserObj = (value:{id:number; name: string})=>{
+  return [value];
+}
