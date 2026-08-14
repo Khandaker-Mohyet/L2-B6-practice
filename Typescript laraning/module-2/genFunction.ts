6a8c3a17d4ec246a89111ecf911f40505ebf731f
Khandaker-Mohyet <mohyet008@gmail.com>
2026-08-15 02:33:47 +0600

    constraint

--- Typescript laraning/module-2/genFunction.ts
+++ Typescript laraning/module-2/genFunction.ts
@@ -10,3 +10,24 @@ const arrayWithUserObj = (value:{id:number; name: string})=>{
 const arryWithGen = <T>(value:T) =>{
   return [value]
 }
+
+// constraint: strict rules deya
+
+const addStudentToCourse = <T extends{id:number, name: string}>(studentInfo:T)=>{
+  return{
+    course: "Next Level",
+    ...studentInfo,
+  }
+}
+
+const student1 = {
+  id: 123,
+  name: "Khandaker",
+  hasPen: true
+}
+const student2 = {
+  id: 524,
+  name: "Mohyet",
+  hasCar: true,
+  isMarried: true,
+}
