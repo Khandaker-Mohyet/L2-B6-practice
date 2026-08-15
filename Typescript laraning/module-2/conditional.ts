ebcbf5f0d8b662a5e7e42ff5cc9f1daec0e3d892
Khandaker-Mohyet <mohyet008@gmail.com>
2026-08-15 17:34:42 +0600

    enam

---
+++ Typescript laraning/module-2/conditional.ts
@@ -0,0 +1,4 @@
+type A = null;
+type B = undefined;
+
+type C = A extends number ? true: B extends undefined ? true: false;
---
+++ Typescript laraning/module-2/enum.ts
@@ -0,0 +1,7 @@
+ 
+
+ enum UserRoles{
+  Admin = "Admin",
+  Editor = "Editor",
+  Viewer = "Viewer"
+ }
