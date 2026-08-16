e1cd4e7022bd03c2cebd3425839489cbf0133873
Khandaker-Mohyet <mohyet008@gmail.com>
2026-08-17 01:51:45 +0600

    encoplotation

---
+++ Typescript laraning/module-3/encapsulation.ts
@@ -0,0 +1,20 @@
+class BankAccount {
+  readonly userId: number;
+  name: string;
+  private userBalance: number; // protected have
+
+  constructor(userId: number, name: string, userBalance: number) {
+    this.userId = userId;
+    this.name = name;
+    this.userBalance = userBalance;
+  }
+
+  addBalance(balance: number) {
+    this.userBalance = this.userBalance + balance
+  }
+}
+
+const myAccount = new BankAccount(12123, "Mohyet", 200)
+
+myAccount.addBalance(30000)
+console.log(myAccount)
